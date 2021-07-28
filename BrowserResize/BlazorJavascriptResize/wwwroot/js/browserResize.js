e7a8d475d4a39217d3148ebbafe13b54b03b2ba2
alistairdevine <alistair.devine98@gmail.com>
2021-07-28 12:23:45 +0100

    Blazor Javascript testing environment working
    Testing: St Barnabas Hospice project, table template javascript DOM resizing process.
    
    Result: Found a completely working solution to the problem.

---
+++ BrowserResize/BlazorJavascriptResize/wwwroot/js/browserResize.js
@@ -0,0 +1,29 @@
+﻿var browserResize = browserResize || {}
+
+browserResize.getInnerWidth = function () {
+    const surveyCom = document.querySelector('.mt-4');
+    return surveyCom.offsetWidth;
+}
+
+
+browserResize.getInnerHeight = function () {
+    const surveyCom = document.querySelector('.mt-4');
+    return surveyCom.offsetHeight;
+}
+
+browserResize.getSurveySize = function () {
+    const surveyCom = document.querySelector('.mt-4');
+    return {
+        width: surveyCom.offsetWidth,
+        height: surveyCom.offsetHeight
+    };
+}
+
+browserResize.registerResizeCallback = function () {
+    window.addEventListener("resize", browserResize.resized);
+}
+
+browserResize.resized = function () {
+    //DotNet.invokeMethod("BrowserResize", 'OnBrowserResize');
+    DotNet.invokeMethodAsync("BlazorJavascriptResize", 'OnBrowserResize').then(data => data);
+}
