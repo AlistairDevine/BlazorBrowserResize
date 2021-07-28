06492f6939483b4cea42c7026432116a8dda7051
alistairdevine <alistair.devine98@gmail.com>
2021-07-28 16:50:41 +0100

    Implementation of the Resize observer Api
    Working: watch a DOM element resizing inside the window.

--- BrowserResize/BlazorJavascriptResize/wwwroot/js/browserResize.js
+++ BrowserResize/BlazorJavascriptResize/wwwroot/js/browserResize.js
@@ -27,3 +27,15 @@ browserResize.resized = function () {
     //DotNet.invokeMethod("BrowserResize", 'OnBrowserResize');
     DotNet.invokeMethodAsync("BlazorJavascriptResize", 'OnBrowserResize').then(data => data);
 }
+
+browserResize.ApiObserver = function () {
+    const myObserver = new ResizeObserver(entries => {
+        entries.forEach(entry => {
+            console.log('width', entry.contentRect.width);
+            console.log('height', entry.contentRect.height);
+        });
+    });
+
+    const someEl = document.querySelector('.mt-4');
+    myObserver.observe(someEl);
+}
